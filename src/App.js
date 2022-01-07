492c9365d220e91f7e79d08935c5aabeb6bc8de0
bvenceslas <bvenceslas@gmail.com>
2022-01-07 11:01:36 +0200

    home component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import CreateContact from './components/CreateContact/CreateContact.jsx';
 import Home from './components/Home/Home.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 
-
 function App() {
   return (
-    <Router>
         <div className="App">
           <Navbar />
           <div className="content">
             {/* Our App Data here */}
             <Routes>
-              <Route exact path="/" component={Home} />
-              {/* <Route exact path="/add-contact" component={CreateContact} /> */}
-              {/* <Route exact path='*' component={NotFount} /> */}
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/add-contact" element={<CreateContact />} />
+              {/* <Route exact path='*' element={<NotFount />} /> */}
             </Routes>
           </div>
-        </div>
-    </Router>
-    
+        </div>    
   );
 }
 
---
+++ src/components/CreateContact/CreateContact.jsx
@@ -0,0 +1,9 @@
+const CreateContact = () => {
+    return ( 
+        <div className="create-contact">
+            <h3>Create Contacts</h3>
+        </div>
+     );
+}
+ 
+export default CreateContact;
